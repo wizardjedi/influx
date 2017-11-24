d87d62c84efaf9b07c284dbd39d7520ad8539a18
Sayapin Alexander <a.sayapin@a1-systems.com>
2017-11-24 17:57:56 +0300

    * fix

--- src/influx.erl
+++ src/influx.erl
@@ -1,8 +1,11 @@
 -module(influx).
--export([send/2, send/3]).
+-export([send/2, send/3, send/4]).
 
 send(Measurement, Fields) ->
 	influx_udp:send( influx_line:encode_simple(Measurement, Fields) ).
 
 send(Measurement, Fields, Tags) ->
 	influx_udp:send(influx_line:encode(Measurement, Fields, Tags)).
+
+send(Measurement, Fields, Tags, Timestamp) ->
+	influx_udp:send(influx_line:encode(Measurement, Fields, Tags, Timestamp)).
